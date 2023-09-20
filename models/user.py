f1cd6dead6eaf42f0c6ca1bd1a1d53248d91c85d
babafemiolatona <femiolatona@gmail.com>
2023-09-20 20:55:13 +0100

    Updated user model

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class User that inherit from baseModel and Base"""
+"""This module defines a class User that inherit from BaseModel and Base"""
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
@@ -7,10 +7,10 @@ from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes and relationships"""
-        __tablename__ = "users"
-        email = (Column(String(128), nullable=False))
-        password = (Column(String(128), nullable=False))
-        first_name = (Column(String(128)))
-        last_name = (Column(String(128)))
-        places = (relationship("Place", backref="user", cascade="delete"))
-        reviews = (relationship("Review", backref="user", cascade="delete"))
+    __tablename__ = "users"
+    email = (Column(String(128), nullable=False))
+    password = (Column(String(128), nullable=False))
+    first_name = (Column(String(128), nullable=False))
+    last_name = (Column(String(128), nullable=False))
+    # places = (relationship("Place", backref="user", cascade="delete"))
+    # reviews = (relationship("Review", backref="user", cascade="delete"))
