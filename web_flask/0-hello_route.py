d5ff3874927c994e61f2eb68e2a4a9c9c8c5ae9f
bolexs <boluwatufeadeojo@gmail.com>
2023-10-22 20:55:43 +0100

    web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """Start a Flask web application"""
 from flask import Flask
+
+
 app = Flask(__name__)
+
+
 
 @app.route("/", strict_slashes=False)
 def hello_hb():
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts the web app"""
+"""Adding more routes to the web app"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,11 +9,14 @@ app = Flask(__name__)
 def hello_hb():
     return "Hello HBNB!"
 
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
