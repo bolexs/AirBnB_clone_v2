3f395c773934bee97841dc97a92e28d7e32b8eb9
bolexs <boluwatufeadeojo@gmail.com>
2023-10-22 20:58:01 +0100

    web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,9 +2,7 @@
 """Start a Flask web application"""
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 
 @app.route("/", strict_slashes=False)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,14 +9,17 @@ app = Flask(__name__)
 def hello_hb():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     text = text.replace("_", " ")
     return f"C {text}"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
